f7f0d7c6c73e13894ea20a25b69e873d88867235
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2020-12-27 22:24:43 -0500

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FacebookService, InitParams } from "ngx-facebook";
+import { SocialAuthService } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
 
 @Component({
   selector: 'app-root',
@@ -7,13 +9,21 @@ import { FacebookService, InitParams } from "ngx-facebook";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private facebookService: FacebookService) { }
+  constructor(private facebookService: FacebookService, private authService: SocialAuthService) { }
   ngOnInit(): void {
     this.initFacebookService();
   }
   private initFacebookService(): void {
     const initParams: InitParams = { xfbml:true, version:'v3.2'};
     this.facebookService.init(initParams);
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+  
+  signOut(): void {
+    this.authService.signOut();
   }
 
   
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FacebookModule } from "ngx-facebook";
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import { GoogleLoginProvider } from 'angularx-social-login';
+
 
 @NgModule({
   declarations: [
@@ -13,9 +16,25 @@ import { FacebookModule } from "ngx-facebook";
     BrowserModule,
     MDBBootstrapModule.forRoot(),
     FacebookModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '480702026097-19viqflvotntfpksgd4er5pqav3nefnd.apps.googleusercontent.com'
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
