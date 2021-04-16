82df35ee9747b6ee6a730a146dc76fc246e50f28
miguelparra2020 <miguel.parra@correounivalle.edu.co>
2021-04-16 14:59:00 -0500

    Updated

--- src/app/vision/vision.module.ts
+++ src/app/vision/vision.module.ts
@@ -4,13 +4,19 @@ import { CommonModule } from '@angular/common';
 import { VisionRoutingModule } from './vision-routing.module';
 import { VisionPageComponent } from './vision-page/vision-page.component';
 import { HeaderComponent } from './header/header.component';
+import { MDBBootstrapModule } from 'projects/angular-bootstrap-md/src/public_api';
+import { NgwWowModule } from 'ngx-wow';
+import { HomeComponentsModule } from '../home-components/home-components.module';
 
 
 @NgModule({
   declarations: [VisionPageComponent, HeaderComponent],
   imports: [
     CommonModule,
-    VisionRoutingModule
+    VisionRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    NgwWowModule,
+    HomeComponentsModule
   ]
 })
 export class VisionModule { }
